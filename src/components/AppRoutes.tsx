6cfedde8a18996d53de4caac37e19db872294781
Eduard <edwardak00@gmail.com>
2022-06-25 13:11:46 +0300

    enable absolute path for imports

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -1,6 +1,6 @@
 import { RouteObject, useRoutes } from 'react-router-dom';
 
-import HomePage from '../pages';
+import HomePage from 'pages';
 
 const AppRoutes: React.FC = () => {
   const routes: RouteObject[] = [
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import './index.css';
+import 'index.css';
 
-import AppRoutes from './components/AppRoutes';
+import AppRoutes from 'components/AppRoutes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
