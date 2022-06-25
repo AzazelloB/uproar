738a899566aa260d8ff2f4e40674bc616a2bda5a
Eduard <edwardak00@gmail.com>
2022-06-25 13:08:03 +0300

    add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,12 @@ import './index.css';
 import AppRoutes from './components/AppRoutes';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AppRoutes />
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
